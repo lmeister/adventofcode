a11a78e0c33b15156b9cc0c608bdb71a4861dde9
Leon Meister <Leonmeister97@gmail.com>
2024-11-14 17:09:18 +0100

    2019-Day02 part2 bruteforce solution

--- src/main/adventofcode/year2019/days/Day02.java
+++ src/main/adventofcode/year2019/days/Day02.java
@@ -5,6 +5,7 @@ import main.adventofcode.framework.InputReader;
 import main.adventofcode.year2019.util.IntcodeComputer;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Day02 implements Day {
     private String inputFilePath;
@@ -15,6 +16,7 @@ public class Day02 implements Day {
     public String solvePart1() {
         try {
             IntcodeComputer intcodeComputer = new IntcodeComputer(InputReader.readStringFromFile(inputFilePath));
+            intcodeComputer.initializeProgramState(12, 2);
             return String.valueOf(intcodeComputer.runProgram().get(0));
         } catch (IOException ioException) {
             ioException.printStackTrace();
@@ -23,6 +25,25 @@ public class Day02 implements Day {
     }
 
     public String solvePart2() {
-        return null;
+
+        try {
+            IntcodeComputer intcodeComputer = new IntcodeComputer(InputReader.readStringFromFile(inputFilePath));
+
+            for (int noun = 0; noun <= 99; noun++) {
+                for (int verb = 0; verb <= 99; verb++) {
+                    intcodeComputer.initializeProgramState(noun, verb);
+                    List<Integer> result = intcodeComputer.runProgram();
+
+                    if (result.get(0) == 19690720) {
+                        return String.valueOf(100 * noun + verb);
+                    }
+                }
+            }
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+            return "Error reading file: " + ioException.getMessage();
+        }
+
+        return "No Solution found.";
     }
 }
--- src/main/adventofcode/year2019/util/IntcodeComputer.java
+++ src/main/adventofcode/year2019/util/IntcodeComputer.java
@@ -6,62 +6,67 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class IntcodeComputer {
-    private List<Integer> program;
+    private List<Integer> memory;
+    private List<Integer> currentState;
 
 
-    public IntcodeComputer(List<Integer> program) {
-        this.program = new ArrayList<>(program); // copy in order to maintain original list just in case
+    public IntcodeComputer(List<Integer> memory) {
+        this.memory = new ArrayList<>(memory); // copy in order to maintain original list just in case
     }
 
-    public IntcodeComputer(String program) {
-        this.program = Arrays.stream(program.split(","))
+    public IntcodeComputer(String memory) {
+        this.memory = Arrays.stream(memory.split(","))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
     }
 
+    public void initializeProgramState(int noun, int verb) {
+        this.currentState = new ArrayList<>(this.memory); // Reset to initial state/memory
+        this.currentState.set(1, noun);
+        this.currentState.set(2, verb);
+    }
+
     public List<Integer> runProgram() {
-        // Return computer to state of crashed computer
-        this.program.set(1, 12);
-        this.program.set(2, 2);
         int pointer = 0;
 
-        while (pointer < this.program.size()) {
-            int opcode = this.program.get(pointer);
+        while (pointer < this.currentState.size()) {
+            int opcode = this.currentState.get(pointer);
 
             // Halt program execution on opcode 99
             if (opcode == 99) {
                 break;
             }
 
-            if (pointer + 3 >= this.program.size()) {
+            if (pointer + 3 >= this.currentState.size()) {
                 throw new IndexOutOfBoundsException("Incomplete opcode parameters after pointer " + pointer);
             }
 
-            int address1 = this.program.get(pointer + 1);
-            int address2 = this.program.get(pointer + 2);
-            int targetAddress = this.program.get(pointer + 3);
+            int address1 = this.currentState.get(pointer + 1);
+            int address2 = this.currentState.get(pointer + 2);
+            int targetAddress = this.currentState.get(pointer + 3);
 
             runOpcode(opcode, address1, address2, targetAddress);
             pointer += 4; // Move to next instruction set
         }
 
-        return this.program;
+        return this.currentState;
     }
 
     private void runOpcode(int operation, int address1, int address2, int targetAddress) {
-        if (address1 >= this.program.size() || address2 >= this.program.size() || targetAddress >= this.program.size()) {
+        if (address1 >= this.currentState.size() || address2 >= this.currentState.size()
+                || targetAddress >= this.currentState.size()) {
             String errorMessage = String.format(
                     "Address out of bounds:\nAddress1=[%d], Address2=[%d], TargetAddress=[%d], Programsize=[%d]",
-                    address1, address2, targetAddress, this.program.size());
+                    address1, address2, targetAddress, this.currentState.size());
             throw new IndexOutOfBoundsException(errorMessage);
         }
 
         switch (operation) {
             case 1:
-                this.program.set(targetAddress, this.program.get(address1) + this.program.get(address2));
+                this.currentState.set(targetAddress, this.currentState.get(address1) + this.currentState.get(address2));
                 break;
             case 2:
-                this.program.set(targetAddress, this.program.get(address1) * this.program.get(address2));
+                this.currentState.set(targetAddress, this.currentState.get(address1) * this.currentState.get(address2));
                 break;
             case 99:
                 // Do nothing, program should halt (Handled in runProgram)
